cd496883d83f3b4d66b82e706f01ce7964e401ed
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-01-01 23:36:04 +0300

    feat(options): add async options interfaces

--- lib/interfaces/etcd-options.interface.ts
+++ lib/interfaces/etcd-options.interface.ts
@@ -1,1 +1,18 @@
-export { IOptions as EtcdModuleOptions } from 'etcd3';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Type } from '@nestjs/common';
+import { ModuleMetadata } from '@nestjs/common/interfaces';
+import { IOptions } from 'etcd3';
+
+export type EtcdModuleOptions = IOptions;
+
+export interface EtcdOptionsFactory {
+  createEtcdOptions(connectionName?: string): Promise<EtcdModuleOptions> | EtcdModuleOptions;
+}
+
+export interface EtcdModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+  name?: string;
+  useExisting?: Type<EtcdOptionsFactory>;
+  useClass?: Type<EtcdOptionsFactory>;
+  useFactory?: (...args: any[]) => Promise<EtcdModuleOptions> | EtcdModuleOptions;
+  inject?: any[];
+}
