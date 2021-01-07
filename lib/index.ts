fe640acb8dc19bd2ddcd37d642cb7a0576bbaa3a
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-01-07 16:03:50 +0300

    feat(utils): export utils from the module

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
-export * from './etcd.decorators';
 export * from './interfaces';
+export * from './etcd.constants';
+export * from './etcd.decorators';
 export * from './etcd.module';
+export * from './etcd.utils';
 export { Etcd3 } from 'etcd3';
